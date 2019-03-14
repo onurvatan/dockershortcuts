fb675018336e4532f49cb898fe4aca0107ef412a
onurvatan <onurr.vatan@gmail.com>
2019-03-14 13:46:34 +0300

    Update dockercommands.ps1

--- dockercommands.ps1
+++ dockercommands.ps1
@@ -25,5 +25,19 @@ docker stop $(docker ps -a -q)
 #remove all containers
 docker rm $(docker ps -a -q)
 #docker compose yml or yaml file
-docker-compose up
+docker-compose up -d
 docker-compose down
+#filter images
+$ docker images --filter "label=com.example.version"
+#images format
+docker images --format "{{.ID}}: {{.Repository}}"
+
+#elastic search run
+docker network create somenetwork
+docker run -d --name elasticsearch --net somenetwork -p 9200:9200 -p 9300:9300 
+docker pull docker.elastic.co/elasticsearch/elasticsearch:6.6.2
+docker run -p 9200:9200 -p 9300:9300 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:6.6.2
+
+$ docker run -i -t ubuntu:12.04 /bin/bash
+
+docker-compose up -d --force-recreate --build
